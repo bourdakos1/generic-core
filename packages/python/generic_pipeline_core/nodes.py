0485c2145d2d62c565be224712405ff4e28c10b1
Nick Bourdakos <bourdakos1@gmail.com>
2021-04-25 15:50:48 -0400

    add python package

--- packages/python/generic_pipeline_core/nodes.py
+++ packages/python/generic_pipeline_core/nodes.py
@@ -7,10 +7,6 @@ config_path = os.path.join(dist_path, "config.json")
 with open(config_path) as f:
   config = json.load(f)
 
-print(config)
-
 nodes_path = os.path.join(dist_path, "nodes.json")
 with open(nodes_path) as f:
   nodes = json.load(f)
-
-print(nodes)
---
+++ packages/python/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='generic-pipeline-core',
+    version='0.0.1',
+    include_package_data=True,
+    packages=find_packages()
+)
