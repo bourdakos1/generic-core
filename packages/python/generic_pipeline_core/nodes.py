b8a65482cdc50d907a679878a9b52c384bbc0b64
Nick Bourdakos <bourdakos1@gmail.com>
2021-04-26 10:13:46 -0400

    add some python witchcraft

--- packages/python/generic_pipeline_core/nodes.py
+++ packages/python/generic_pipeline_core/nodes.py
@@ -1,12 +1,23 @@
 import os
 import json
 
-dist_path = os.path.join(os.path.dirname(__file__), "../../../dist")
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
 
-config_path = os.path.join(dist_path, "config.json")
-with open(config_path) as f:
-  config = json.load(f)
+from . import dist
 
-nodes_path = os.path.join(dist_path, "nodes.json")
-with open(nodes_path) as f:
-  nodes = json.load(f)
+
+nodes = json.loads(pkg_resources.read_text(dist, 'nodes.json'))
+
+# dist_path = os.path.join(os.path.dirname(__file__), "dist")
+
+# config_path = os.path.join(dist_path, "config.json")
+# with open(config_path) as f:
+#   config = json.load(f)
+
+# nodes_path = os.path.join(dist_path, "nodes.json")
+# with open(nodes_path) as f:
+#   nodes = json.load(f)
--- packages/python/setup.py
+++ packages/python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='generic-pipeline-core',
+    name='generic_pipeline_core',
     version='0.0.1',
     include_package_data=True,
     packages=find_packages()
